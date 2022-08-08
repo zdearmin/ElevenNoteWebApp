fc8e0dd61fb44f237658ed3b702f57bc61725433
Zach Dearmin <zacharydearmin@gmail.com>
2022-08-08 10:28:46 -0400

    Shared Note model classes added

---
+++ ElevenNoteWebApp/Shared/Models/Note/NoteCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNoteWebApp.Shared.Models.Note
+{
+    internal class NoteCreate
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ ElevenNoteWebApp/Shared/Models/Note/NoteListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNoteWebApp.Shared.Models.Note
+{
+    internal class NoteListItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string CategoryName { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}

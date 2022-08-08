ef2b695df2dfafa58fb3be25d3bf0c3590e92596
Zach Dearmin <zacharydearmin@gmail.com>
2022-08-08 10:10:44 -0400

    Category model class added

--- ElevenNoteWebApp/Server/Models/Note.cs
+++ ElevenNoteWebApp/Server/Models/Note.cs
@@ -1,6 +1,25 @@
-﻿namespace ElevenNoteWebApp.Server.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNoteWebApp.Server.Models
 {
     public class Note
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string OwnerId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+        public int CategoreyId { get; set; }
+        public virtual Category Category { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
